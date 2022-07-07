03263db124b44e9b4cb13c00156edc12dd2de05d
thechiefpotatopeeler <jacksth@whs.school.nz>
2022-07-08 09:30:10 +1200

    Added comments

--- main.js
+++ main.js
@@ -4,46 +4,45 @@
 //License: Creative Commons Attribution-NonCommercial-NoDerivs 2.0
 //Language: javascript
 
-const CANVAS_WIDTH = 1000;
-const CANVAS_HEIGHT = 500;
-const HEALTH_POS_X = 10;
-const HEALTH_POS_Y = 10;
-const HEALTH_SIZE = 10;
-const PLAYER_SIZE = 16;
-const MAX_ATTACK_POWER = 100;
-const GAME_OVER_TEXT = "Game Over";
+const CANVAS_WIDTH = 1000;//This constant is the canvas width
+const CANVAS_HEIGHT = 500;//This constant is the canvas height
+const HEALTH_POS_X = 10;//This constant is the x position of the health
+const HEALTH_POS_Y = 10;//This constant is y position of the health
+const HEALTH_SIZE = 10;//This constant is the size of the health
+const PLAYER_SIZE = 16;//This constant is the size of the player
+const MAX_ATTACK_POWER = 100;//This constant is the maximum attack power
+const GAME_OVER_TEXT = "Game Over";//This is the text for the game over game state
 
-const PLAYER_1_IMAGE = new Image();
-const PLAYER_2_IMAGE = new Image();
-const ENEMY_IMAGE = new Image();
-const START_BUTTON_IMAGE = new Image();
-const LOGO_IMAGE = new Image();
-const PAUSE_BUTTON_IMAGE = new Image();
+const PLAYER_1_IMAGE = new Image();//This is the first player image
+const PLAYER_2_IMAGE = new Image();//This is the second player image
+const ENEMY_IMAGE = new Image();//This is the image for the enemy
+const START_BUTTON_IMAGE = new Image();//This is the image for the start button
+const LOGO_IMAGE = new Image();//This is the image for the logo
+const PAUSE_BUTTON_IMAGE = new Image();//This is the image for the pause button
 
 
-PLAYER_1_IMAGE.src = "images/player1.png";
-PLAYER_2_IMAGE.src = "images/player2.png";
-ENEMY_IMAGE.src = "images/enemy.png";
-START_BUTTON_IMAGE.src = "images/startButton.png";
-LOGO_IMAGE.src = "images/logo.png";
-PAUSE_BUTTON_IMAGE.src = "images/pauseButton.png";
+PLAYER_1_IMAGE.src = "images/player1.png";//This is the source for the first player image
+PLAYER_2_IMAGE.src = "images/player2.png";//This is the source for the second player image
+ENEMY_IMAGE.src = "images/enemy.png";//This is the source for the enemy image
+START_BUTTON_IMAGE.src = "images/startButton.png";//This is the source for the start button image
+LOGO_IMAGE.src = "images/logo.png";//This is the source for the logo image
+PAUSE_BUTTON_IMAGE.src = "images/pauseButton.png";//This is the source for the pause button image
 
-var lives = 3;
-var playerSpeed = 1.25;
-var rightPressed = false;
-var leftPressed = false;
-var upPressed = false;
-var downPressed = false;
-var attackPressed = false;
-var beamSize = 5;
-var attackPower = 100;
-var backUpAttack = 100;
-var enemySpeed = 0.5;
-var attack
-var score = 0;
+var lives = 3;//This is the number of lives
+var playerSpeed = 1.25;//This is the player's speed coefficient
+var rightPressed = false;//This variable is for right button press
+var leftPressed = false;//This variable is for left button press
+var upPressed = false;//This variable is for up button press
+var downPressed = false;//This variable is for down button press
+var attackPressed = false;//This variable is for attack button press
+var beamSize = 5;//This variable is for beam size
+var attackPower = 100;//This variable is for attack power
+var enemySpeed = 0.5;//This is the enemy's speed coeffficient
+var attack//This is the attack variable (boolean)
+var score = 0;//This is the score variable
 var gameState = "photosensitiveWarning";//Sets the gameState to menu
-var photosensitiveMode = false;
-class GameObject{
+var photosensitiveMode = false;//This is the variable for photosensitive mode
+class GameObject{//This is the GameObject class, for any object that needs positions, sizes, drawing, or movement
     constructor(id,image,x,y,width,height){//Constructor for the entity, had the identifier, texture, coordinates, and size
         this.id=id;//Sets the identifier
         this.image=image;//Sets the texture
@@ -54,16 +53,16 @@ class GameObject{
     }
 
     setPosition(x,y){//Sets the position of the entity
-        this.x=x;
-        this.y=y;
+        this.x=x;//Sets the x coordinate
+        this.y=y;//Sets the y coordinate
     }
     draw(){//Draws the entity
         //console.log(player);
         //ctx.fillRect(this.x,this.y,this.width,this.height);
-        ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
+        ctx.drawImage(this.image,this.x,this.y,this.width,this.height);//Draws the entity
     }
-    move(direction, distance){
-        switch(direction){
+    move(direction, distance){//Moves the entity
+        switch(direction){//Checks the direction
             case "right": //Moves the player to the right
                 this.x+=distance;
                 break;
@@ -80,13 +79,13 @@ class GameObject{
     }
 }
 const MENU_LOGO = new GameObject("menuLogo",LOGO_IMAGE,CANVAS_WIDTH/2-508/2,2*80/4,508,80);//Creates the menu logo
-const MENU_START_BUTTON = new GameObject("menuStartButton",START_BUTTON_IMAGE,CANVAS_WIDTH/2-160/2,CANVAS_HEIGHT/2-160/2,160,160);
-const PAUSE_BUTTON = new GameObject("pauseButton",PAUSE_BUTTON_IMAGE,CANVAS_WIDTH-(16*2),0+(16),16,16);
+const MENU_START_BUTTON = new GameObject("menuStartButton",START_BUTTON_IMAGE,CANVAS_WIDTH/2-160/2,CANVAS_HEIGHT/2-160/2,160,160);//Creates the start button
+const PAUSE_BUTTON = new GameObject("pauseButton",PAUSE_BUTTON_IMAGE,CANVAS_WIDTH-(16*2),0+(16),16,16);//This is the pause button
 //var entities = [];
 var player1 = new GameObject("player1",PLAYER_1_IMAGE,(CANVAS_WIDTH/4)-PLAYER_SIZE,CANVAS_HEIGHT/2,PLAYER_SIZE,PLAYER_SIZE); //Makes the player's first GameObject
 var player2 = new GameObject("player2",PLAYER_2_IMAGE,3*(CANVAS_WIDTH/4),CANVAS_HEIGHT/2,PLAYER_SIZE,PLAYER_SIZE);//Makes the player's second GameObject
-var currentEnemies=[];
-let coordsAttack = [];
+var currentEnemies=[];//This is the array where the enemies are stored
+// let coordsAttack = [];
 //entities.push(player);
 
 function getPlayerDistance(){
@@ -94,20 +93,20 @@ function getPlayerDistance(){
 }
 function playerAttack(){
     ctx.beginPath();//Starts the path
-    ctx.lineWidth = beamSize
-    if(player1.x < player2.x){
+    ctx.lineWidth = beamSize//Sets the line width to that of the game
+    if(player1.x < player2.x){//Checks if the player is to the left of the other player
         ctx.moveTo(player1.x + PLAYER_SIZE/2,player1.y + PLAYER_SIZE/2);//Moves the path to the player's position
         ctx.lineTo(player2.x+PLAYER_SIZE/2,player2.y+PLAYER_SIZE/2);//Draws a line to the player's position
-    } else if(player1.x > player2.x){
-        ctx.moveTo(player1.x+PLAYER_SIZE/2, player1.y+PLAYER_SIZE/2)
-        ctx.lineTo(player2.x+PLAYER_SIZE/2, player2.y+PLAYER_SIZE/2)
+    } else if(player1.x > player2.x){//Checks if the player is to the right of the other player
+        ctx.moveTo(player1.x+PLAYER_SIZE/2, player1.y+PLAYER_SIZE/2)//Moves the path to the player's position
+        ctx.lineTo(player2.x+PLAYER_SIZE/2, player2.y+PLAYER_SIZE/2)//Draws a line to the player's position
     }
     ctx.strokeStyle = "white";//Sets the color to red
     ctx.stroke();//Fills the line
     attackPower-=2;//Reduces the attack power
 }
-function damagePlayer(){
-    if(photosensitiveMode!=true){
+function damagePlayer(){//This function damages the player
+    if(photosensitiveMode!=true){//Checks if the player is not in photosensitive mode so that the flashes can happen
         ctx.fillStyle = "white"; // Set the color to white
         ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);//Makes a flash of white
     }
@@ -120,28 +119,28 @@ function damagePlayer(){
 }
 
 function closestPlayer(object){//Uses pythagoras theorem to find the closest player
-    if(Math.sqrt(Math.pow(player1.x-object.x,2)+Math.pow(player1.y-object.y,2))<Math.sqrt(Math.pow(player2.x-object.x,2)+Math.pow(player2.y-object.y,2))){
+    if(Math.sqrt(Math.pow(player1.x-object.x,2)+Math.pow(player1.y-object.y,2))<Math.sqrt(Math.pow(player2.x-object.x,2)+Math.pow(player2.y-object.y,2))){//Checks if the player is closer to the first player
         return player1;
     } else {
         return player2;
     }
 }
 
-function trackPlayer(object){
+function trackPlayer(object,speed){//This makes objects follow the first player
     if(object.x>player1.x){
-        object.move("left",enemySpeed);//Moves the enemy to the left if the player is to the left
+        object.move("left",speed);//Moves the enemy to the left if the player is to the left
     } else if(object.x<player1.x){
-        object.move("right",enemySpeed);//Moves the enemy to the right if the player is to the right
+        object.move("right",speed);//Moves the enemy to the right if the player is to the right
     }
     if(object.y>player1.y){
-        object.move("up",enemySpeed);//Moves the enemy up if the player is above
+        object.move("up",speed);//Moves the enemy up if the player is above
     } else if(object.y<player1.y){
-        object.move("down",enemySpeed);//Moves the enemy down if the player is below
+        object.move("down",speed);//Moves the enemy down if the player is below
     }
 }
 
 
-function playerCollision(object){
+function playerCollision(object){//Checks if an object collides with teh player
     if(object.x<player1.x+PLAYER_SIZE && object.x+object.width>player1.x && object.y<player1.y+PLAYER_SIZE && object.y+object.height>player1.y){
         return true; //Returns true if the player1 GameObject is colliding with the object
     } else if(object.x<player2.x+PLAYER_SIZE && object.x+object.width>player2.x && object.y<player2.y+PLAYER_SIZE && object.y+object.height>player2.y){
@@ -151,8 +150,7 @@ function playerCollision(object){
     }
 }
 
-function generateEnemies(number){
-
+function generateEnemies(number){//This function generates enemies with the input number
     for(i=0;i<number;i++){//Generates the enemies
         currentEnemies.push(new GameObject("enemy"+i,ENEMY_IMAGE,Math.random()*CANVAS_WIDTH,Math.random()*CANVAS_HEIGHT,PLAYER_SIZE,PLAYER_SIZE));
     }
@@ -160,8 +158,8 @@ function generateEnemies(number){
 
 function doEnemies(){//Moves and draws the enemies
     for(i=0;i<currentEnemies.length;i++){
-        currentEnemies[i].draw();
-        trackPlayer(currentEnemies[i]);
+        currentEnemies[i].draw();//Draws the enemy
+        trackPlayer(currentEnemies[i],enemySpeed);//Moves the enemy in the direction of the first player
         if(playerCollision(currentEnemies[i])){//If the enemy collides with the player
             damagePlayer();//The player gets damaged
         }
@@ -169,24 +167,24 @@ function doEnemies(){//Moves and draws the enemies
 }
 
 function attackProcedures(){//This function governs everything related to the attack that needs to be exectuted every frame.
-    if(attackPressed == true&&attackPower>0&&(player1.y-player2.y==0)){
+    if(attackPressed == true&&attackPower>0&&(player1.y-player2.y==0)){//Checks if the attack button is pressed and if the attack power is greater than 0 and if the players are on the same y-axis
         attack = true
-    }else if(attackPressed == true&&attackPower>0&&((player1.x-player2.x)<5&&(player1.x-player2.x)>-5)){
+    }else if(attackPressed == true&&attackPower>0&&((player1.x-player2.x)<5&&(player1.x-player2.x)>-5)){//Checks if the attack button is pressed and if the attack power is greater than 0 and if the players are on the same x-axis
         attack = true
     }else{
         attack = false
     }//Executes the player's attack
 
     //This is where the 'soft' attack cooldown is executed
-    if(attackPower < 0){
+    if(attackPower < 0){//Checks if the attack power is less than 0
         attackPower = 0; //Stops attack power from going below 0
-    } else if(attackPower > 100){
+    } else if(attackPower > 100){//Checks if the attack power is greater than 100
         attackPower = 100; //Stops attack power from going above 100
     } else if((attackPower >0||attackPower<100)&&attackPressed!=true){//Regenerates attack power
         attackPower+=0.2;
     }
-    if(attack == true){
-        playerAttack();
+    if(attack == true){//Checks if the player is attacking
+        playerAttack();//Executes the player's attack
     }
     if(attackPower>0){ //Renders the attack power bar
         ctx.fillStyle = "blue";
@@ -236,7 +234,7 @@ function mouseClick(event) {//This function is called when the mouse is clicked
     }
 }
 
-function keyDown(keyboardEvent){
+function keyDown(keyboardEvent){//This function is called when a key is pressed and responds accordingly when certain keys are pressed
     switch(keyboardEvent.key){
         case "d": // Moves the player right
             rightPressed = true;
@@ -283,7 +281,7 @@ function checkAttack(){//Detects if enemies are on the line that runs between th
     }
 }
 
-function keyUp(keyboardEvent){
+function keyUp(keyboardEvent){//This function is called when the key is released and responds if certain keys are pressed
     switch(keyboardEvent.key){
         case "d": //Ends rigth movement
             rightPressed = false;
@@ -303,7 +301,7 @@ function keyUp(keyboardEvent){
     }
 }
 
-function menuLoop(){
+function menuLoop(){//This function is called when the gameState is set to menu and does all the important things for the menu such as text and the buttons
     ctx.fillStyle = "black"; // Set the color to black
     ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);//Makes a black background
     //console.log("menu");
@@ -322,7 +320,7 @@ function menuLoop(){
 function pauseMenuLoop(){
 }
 
-function photosensitiveWarningLoop(){
+function photosensitiveWarningLoop(){//This loop displays all the text for the photosensitive warning
     ctx.fillStyle="black";
     ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
     ctx.fillStyle="red";
@@ -391,8 +389,8 @@ function mainLoop() {
     ctx.font = "30px Arial";
     ctx.fillText(score.toString(),CANVAS_WIDTH/4,HEALTH_POS_Y+25);//Draws the score
 
-    attackProcedures();
-    checkAttack();//This checks if the player's attack hits an enemy
+    attackProcedures();//This runs all the important attack related things like the attack animation and the attack itself
+    checkAttack();//This checks if the player's attack hits an enemy and deals with it accordingly
     player1.draw(); //Draws the first player GameObject
     player2.draw(); //Draws the second player GameObject
     PAUSE_BUTTON.draw();
